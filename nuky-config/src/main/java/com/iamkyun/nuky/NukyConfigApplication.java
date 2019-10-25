4b222b99b553b0a3bafa87720a51b77f122da7be
kyun <yezi0u0@gmail.com>
2019-10-25 13:25:36 +0800

    add config server

---
+++ nuky-config/src/main/java/com/iamkyun/nuky/NukyConfigApplication.java
@@ -0,0 +1,17 @@
+package com.iamkyun.nuky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class NukyConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NukyConfigApplication.class, args);
+    }
+
+}
