f8b750d0b43757c6e5f89a57aa7dabc34034464a
kyun <yezi0u0@gmail.com>
2019-10-29 11:31:00 +0800

    add gateway module

---
+++ nuky-gateway/src/main/java/com/iamkyun/nuky/NukyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.iamkyun.nuky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NukyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NukyGatewayApplication.class, args);
+    }
+
+}
---
+++ nuky-gateway/src/test/java/com/iamkyun/nuky/NukyGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.iamkyun.nuky;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NukyGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
