811008b5debc8395f4ddf679d457487b46614cb8
kyun <yezi0u0@gmail.com>
2020-03-21 16:23:02 +0800

    use mybatis instead of JPA

--- nuky-comment/nuky-comment-service/src/main/java/com/iamkyun/nuky/service/com/iamkyuun/nuky/service/impl/UserServiceImpl.java
+++ nuky-comment/nuky-comment-service/src/main/java/com/iamkyun/nuky/service/com/iamkyuun/nuky/service/impl/UserServiceImpl.java
@@ -11,10 +11,10 @@ import java.util.Optional;
  * @author kyun
  */
 @Service
-public class UserviceImpl implements UserService {
+public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
 
-    public UserviceImpl(UserMapper userMapper) {
+    public UserServiceImpl(UserMapper userMapper) {
         this.userMapper = userMapper;
     }
 
